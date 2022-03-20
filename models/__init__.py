9e9fa6d4422dbc74dcbf4abf1d661abddf523c30
Xu Ma <xuma@my.unt.edu>
2022-03-20 01:58:15 -0400

    Create model2_static_shift.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .poolformer import *
 from .shiftVIT import shiftvit_light_base
 from .model1_static_shift import model1_static_shiftformer_s12
+from .model2_static_shift import model2_static_shiftformer_s12
