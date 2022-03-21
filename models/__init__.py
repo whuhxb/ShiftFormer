6251e6a686d95acb4068e9bea95cd94fdf66606f
Xu Ma <xuma@my.unt.edu>
2022-03-21 00:08:38 -0400

    update

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,5 @@ from .poolformer import *
 from .shiftVIT import shiftvit_light_base
 from .model1_static_shift import model1_static_shiftformer_s12
 from .model2_static_shift import model2_static_shiftformer_s12
+from .model3_static_shift import model3_static_shiftformer_s12
+from .model4_static_shift import model4_static_shiftformer_s12
