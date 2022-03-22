bd2b529181c0564cd9a3e211c66ca41ad3283dc1
Xu Ma <xuma@my.unt.edu>
2022-03-22 15:21:19 -0400

    update

--- run_attentionmap.sh
+++ run_attentionmap.sh
@@ -1,12 +1,14 @@
-for img in demo1.JPEG demo2.JPEG demo3.JPEG demo4.JPEG demo5.JPEG demo6.JPEG
+for img in images/demo1.JPEG images/demo2.JPEG images/demo3.JPEG images/demo4.JPEG images/demo5.JPEG images/demo6.JPEG
 do
-    for Q in 23 83 143
+#    for Q in 23 83 143
+    for Q in 98
     do
         for L in 0 6 11
         do
             for H in 0 3 6 9
             do
-                python3 single.py --image $img --query $Q --layer $L --head $H
+#                echo $img $Q $L $H
+               python3 single.py --image $img --query $Q --layer $L --head $H
             done
         done
     done
