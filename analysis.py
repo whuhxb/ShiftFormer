dfd41df51058a2e933dfe97b6f19fb8c4d8936e9
Xu Ma <xuma@my.unt.edu>
2022-04-06 16:42:36 -0400

    update

--- analysis.py
+++ analysis.py
@@ -7,15 +7,16 @@ import fvcore.common
 from fvcore.nn import FlopCountAnalysis
 
 
-model = models.model1_static_shiftformer_s24()
+model = models.model9_s12_3x3_7x7dilated2()
 
 inputs = (torch.randn((1,3,224,224)),)
 k = 1000.0
 flops = FlopCountAnalysis(model, inputs).total()
-
+print(f"Flops : {flops}")
 flops = flops/(k**3)
 print(f"Flops : {flops:.1f}G")
 params = fvcore.nn.parameter_count(model)[""]
+print(f"Params : {params}")
 params = params/(k**2)
 # print(flops.total())
 print(f"Params : {params:.1f}M")
