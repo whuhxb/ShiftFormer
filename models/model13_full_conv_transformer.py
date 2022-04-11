ae904bf112c9df5b8b4111c8e843ed573d6265ce
Xu Ma <xuma@my.unt.edu>
2022-04-11 17:14:56 -0400

    Update model13_full_conv_transformer.py

--- models/model13_full_conv_transformer.py
+++ models/model13_full_conv_transformer.py
@@ -282,31 +282,32 @@ class BasicBlock(nn.Module):
 
         super().__init__()
 
-        # self.norm1 = norm_layer(dim)
+        self.norm1 = norm_layer(dim)
         self.token_mixer = TokenMixer(dim=dim)
         self.norm2 = norm_layer(dim)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
-                       act_layer=act_layer, drop=drop)
+        self.channel_mixer = ChannelMixer(dim=dim, hidden_dim=mlp_hidden_dim, act_layer=act_layer, drop=drop, useChannelAtt=True)
 
         # The following two techniques are useful to train deep PoolFormers.
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.use_layer_scale = use_layer_scale
         if use_layer_scale:
-            # self.layer_scale_1 = nn.Parameter(
-            #     layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+            self.layer_scale_1 = nn.Parameter(
+                layer_scale_init_value * torch.ones((dim)), requires_grad=True)
             self.layer_scale_2 = nn.Parameter(
                 layer_scale_init_value * torch.ones((dim)), requires_grad=True)
 
     def forward(self, x):
         if self.use_layer_scale:
-            x = self.token_mixer(x)
+            x = x + self.drop_path(
+                self.layer_scale_1.unsqueeze(-1).unsqueeze(-1)
+                * self.token_mixer(self.norm1(x)))
             x = x + self.drop_path(
                 self.layer_scale_2.unsqueeze(-1).unsqueeze(-1)
-                * self.mlp(self.norm2(x)))
+                * self.channel_mixer(self.norm2(x)))
         else:
-            x = x + self.token_mixer(x)
-            x = x + self.drop_path(self.mlp(self.norm2(x)))
+            x = x + self.drop_path(self.token_mixer(self.norm1(x)))
+            x = x + self.drop_path(self.channel_mixer(self.norm2(x)))
         return x
 
 
