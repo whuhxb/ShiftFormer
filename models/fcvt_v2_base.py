b8f205001a1b20b6c468889daa2a37d00b946ca8
Xu Ma <xuma@my.unt.edu>
2022-04-30 22:08:18 -0400

    Update fcvt_v2_base.py

--- models/fcvt_v2_base.py
+++ models/fcvt_v2_base.py
@@ -474,8 +474,8 @@ def fcvt_s12_64(pretrained=False, **kwargs):
 
     fcvt_params = params.copy()
     fcvt_params["spatial_mixer"]["mix_size_1"] = 5
-    fcvt_params["spatial_mixer"]["useSecondTokenMix"] = True
-    fcvt_params["channel_mixer"]["useDWconv"] = False
+    fcvt_params["spatial_mixer"]["useSecondTokenMix"] = False
+    fcvt_params["channel_mixer"]["useDWconv"] = True
     fcvt_params["spatial_mixer"]["useSpatialAtt"] = False
     fcvt_params["channel_mixer"]["useChannelAtt"] = False
 
