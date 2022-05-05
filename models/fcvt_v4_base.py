12dc11f5499e1a9519816e6e7f7a90ddde2c94b2
Xu Ma <xuma@my.unt.edu>
2022-05-05 00:50:53 -0400

    Update fcvt_v4_base.py

--- models/fcvt_v4_base.py
+++ models/fcvt_v4_base.py
@@ -531,7 +531,7 @@ class BaseFormer(nn.Module):
 def fcvt_v4_s12_64_TFFF(pretrained=False, **kwargs):
 
     fcvt_params = params.copy()
-    fcvt_params["spatial_mixer"]["useSecondTokenMix"] = True
+    fcvt_params["spatial_mixer"]["useSecondTokenMix"] = False
     fcvt_params["channel_mixer"]["useDWconv"] = False
     fcvt_params["spatial_mixer"]["useSpatialAtt"] = False
     fcvt_params["channel_mixer"]["useChannelAtt"] = False
