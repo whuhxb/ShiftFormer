1b274b3ef61907c83b8cdebe0414548d1e32e335
Xu Ma <xuma@my.unt.edu>
2022-05-17 18:16:32 -0400

    Update mask_rcnn_fcvt_b12_fpn_1x_coco.py

--- detection/configs/mask_rcnn_fcvt_b12_fpn_1x_coco.py
+++ detection/configs/mask_rcnn_fcvt_b12_fpn_1x_coco.py
@@ -22,4 +22,13 @@ model = dict(
         num_outs=5))
 # optimizer
 optimizer = dict(_delete_=True, type='AdamW', lr=0.0002, weight_decay=0.0001)
-optimizer_config = dict(grad_clip=None)
+# do not use mmdet version fp16
+fp16 = None
+optimizer_config = dict(
+    type="DistOptimizerHook",
+    update_interval=1,
+    grad_clip=None,
+    coalesce=True,
+    bucket_size_mb=-1,
+    use_fp16=True,
+)
