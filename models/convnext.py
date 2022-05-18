455e92f15bc49362a88ed0c9abd258a4d158b340
Xu Ma <xuma@my.unt.edu>
2022-05-18 05:08:59 -0400

    Update convnext.py

--- models/convnext.py
+++ models/convnext.py
@@ -157,10 +157,10 @@ model_urls = {
 @register_model
 def convnext_tiny(pretrained=False,in_22k=False, **kwargs):
     model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
-    # if pretrained:
-    #     url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
-    #     checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
-    #     model.load_state_dict(checkpoint["model"])
+    if pretrained:
+        url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
     return model
 
 @register_model
